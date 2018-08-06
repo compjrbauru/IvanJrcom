1fdeb89dbf33056e88100dc8cb420e526f8cc71b
thoffart <thiagohoffart321920@gmail.com>
2018-08-06 20:21:19 -0300

    Adicionando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './@core/core.module';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ThemeModule } from './@theme/theme.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AdminComponent } from './admin/admin.component';
 
+import { CoreModule } from './@core/core.module';
+import { ThemeModule } from './@theme/theme.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { config } from '../app/config/config';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -23,7 +24,7 @@ import { AdminComponent } from './admin/admin.component';
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-
+    AngularFireModule.initializeApp(config.fire),
     NgbModule.forRoot(),
     ThemeModule.forRoot(),
     CoreModule.forRoot(),
