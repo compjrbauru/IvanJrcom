06ef8c8aaa20a165be9dd14b641e5a091381df38
Tonhao165 <eugenio165.as@gmail.com>
2018-08-09 22:56:22 -0300

    criei modulo

---
+++ src/app/home/show-evento/show-evento.module.ts
@@ -0,0 +1,10 @@
+import { ShowEventoComponent } from './show-evento.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    ShowEventoComponent,
+  ],
+  declarations: [],
+})
+export class ShowEventoModule { }
