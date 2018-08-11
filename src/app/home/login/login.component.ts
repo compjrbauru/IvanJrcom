bd55208775812aaa1157719f7a8fb62e89291311
John David <john.david1321@hotmail.com>
2018-08-11 12:03:00 -0300

    Criado campos de usuário, password e iniciada a lógica com formBuilder

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  form_login: any;
+  constructor(private formBuilder: FormBuilder) {
+  this.form_login = this.formBuilder.group({
+    login: ['',Validators.required],
+    password: ['',Validators.required],
+  });
+   }
   ngOnInit() {
   }
 
