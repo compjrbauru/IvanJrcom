c06059fc8946d7248aef13b181f3f7c9daaec109
John David <john.david1321@hotmail.com>
2018-08-12 23:55:32 -0300

    Realizada alterações na branch, requisito review.

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  form_login: any;
+  form_login: FormGroup;
   constructor(private formBuilder: FormBuilder) {
-  this.form_login = this.formBuilder.group({
-    login: ['',Validators.required],
-    password: ['',Validators.required],
-  });
    }
   ngOnInit() {
+    this.form_login = this.formBuilder.group({
+    login: ['', Validators.required],
+    password: ['', Validators.required],
+  });
   }
-
 }
