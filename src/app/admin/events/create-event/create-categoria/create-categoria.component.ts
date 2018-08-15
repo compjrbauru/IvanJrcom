21a229ca2b47576f1c7dda03c5b74bf94c626760
Vinicius Dias <vinicius.dias@fc.unesp.br>
2018-08-15 08:26:12 -0300

    lint

--- src/app/admin/events/create-event/create-categoria/create-categoria.component.ts
+++ src/app/admin/events/create-event/create-categoria/create-categoria.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-create-categoria',
   templateUrl: './create-categoria.component.html',
-  styleUrls: ['./create-categoria.component.scss']
+  styleUrls: ['./create-categoria.component.scss'],
 })
 export class CreateCategoriaComponent implements OnInit {
 
