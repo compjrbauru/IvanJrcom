26816db1b8e0c736b559dc600fca34509ab0e5d7
thoffart <thiagohoffart321920@gmail.com>
2018-08-16 19:28:38 -0300

    qrcode instalado

--- src/app/home/main/main.module.ts
+++ src/app/home/main/main.module.ts
@@ -1,13 +1,14 @@
+import { NgModule } from '@angular/core';
+import { QRCodeModule } from 'angularx-qrcode';
+
 import { ShowEventoModule } from './../../@core/components/show-evento/show-evento.module';
 import { ThemeModule } from './../../@theme/theme.module';
-import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
 
 const MAIN_COMPONENTS = [MainComponent];
 
 @NgModule({
-    imports: [ThemeModule, ShowEventoModule],
-    declarations: [...MAIN_COMPONENTS],
+  imports: [ThemeModule, ShowEventoModule, QRCodeModule],
+  declarations: [...MAIN_COMPONENTS]
 })
-
 export class MainModule {}
