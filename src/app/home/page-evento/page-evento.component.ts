0e8fc361c458ee8eb7974bcd62dfaf1e1f9a4bf2
Tonhao165 <eugenio165.as@gmail.com>
2018-08-17 12:34:10 -0300

    criei page-evento component

---
+++ src/app/home/page-evento/page-evento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-page-evento',
+  templateUrl: './page-evento.component.html',
+  styleUrls: ['./page-evento.component.scss']
+})
+export class PageEventoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/home/page-evento/page-evento.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageEventoComponent } from './page-evento.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PageEventoComponent]
+})
+export class PageEventoModule { }
