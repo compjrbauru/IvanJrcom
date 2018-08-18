179f3c7d461f5089724525ee0c5ebf36b44841a0
johndavvid <john.david1321@hotmail.com>
2018-08-18 14:40:19 -0300

    Realizada modificações no formLogin

--- src/app/home/login/login.module.ts
+++ src/app/home/login/login.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ThemeModule } from '../../@theme-home/theme.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -7,7 +7,7 @@ import { LoginComponent } from './login.component';
 const LOGIN_COMPONENTS = [LoginComponent];
 
 @NgModule({
-  imports: [LoginRoutingModule, ThemeModule],
+  imports: [LoginRoutingModule, ThemeModule, TooltipModule],
   declarations: [...LOGIN_COMPONENTS],
 })
 export class LoginModule {}
