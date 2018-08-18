f3724ead818670698c734b9ffb62f844e2e34526
Vinicius Dias <vinicius.dias@fc.unesp.br>
2018-08-18 12:31:30 -0300

    Conflitos entre theme e ngprime

--- src/app/home/main/carousel/carousel-local.component.ts
+++ src/app/home/main/carousel/carousel-local.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'ngx-carousel',
   templateUrl: './carousel-local.component.html',
   styleUrls: ['./carousel-local.component.scss'],
 })
 export class CarouselLocalComponent implements OnInit {
-  @Input() eventos: any;
+  @Input() eventos: Observable<any>;
+  eventosShow: any;
+  teste: string = 'teste';
+  item = [
+    {
+      teste: 1,
+    },
+    {
+      teste: 2,
+    },
+  ];
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.eventos);
+    this.eventos.subscribe(response => {
+      this.eventosShow = response;
+      console.log(this.eventosShow);
+    });
   }
 
 }
