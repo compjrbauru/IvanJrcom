d25fd59f99b553f3b55f84595468480ac5cc4ff3
Tonhao165 <eugenio165.as@gmail.com>
2018-08-19 17:46:34 -0300

    arrumando carrinho de compra

--- src/app/home/page-evento/page-evento.component.ts
+++ src/app/home/page-evento/page-evento.component.ts
@@ -12,10 +12,38 @@ export class PageEventoComponent implements OnInit, OnDestroy {
   id: number;
   private sub: any;
   private eventsub: any;
+  private qty: {
+    fem: number,
+    masc: number,
+    uni: number,
+  };
+
+  minus(type: string) {
+    if (type === 'fem')
+      this.qty.fem--;
+    else if (type === 'masc')
+      this.qty.masc--;
+    else if (type === 'uni')
+      this.qty.uni--;
+  }
+
+  plus(type: any) {
+    if (type === 'fem')
+      this.qty.fem++;
+    else if (type === 'masc')
+      this.qty.masc++;
+    else if (type === 'uni')
+      this.qty.uni++;
+  }
 
   constructor(private route: ActivatedRoute, private eventoService: EventoService) { }
 
   ngOnInit() {
+    this.qty = {
+      fem: 0,
+      masc: 0,
+      uni: 0,
+    }
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
       this.eventsub = this.eventoService.getID(this.id).subscribe(response => {
