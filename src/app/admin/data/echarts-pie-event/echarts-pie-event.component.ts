e80b592c14dedb3988a4df8ae94366fd162b0e30
thoffart <thiagohoffart321920@gmail.com>
2018-08-27 20:40:46 -0300

    dados passados para o grafico

--- src/app/admin/data/echarts-pie-event/echarts-pie-event.component.ts
+++ src/app/admin/data/echarts-pie-event/echarts-pie-event.component.ts
@@ -1,82 +1,97 @@
-import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
+import { map, mapKeys, pick } from 'lodash';
+
+import { CategoriaService } from './../../../services/categoria.service';
 
 @Component({
   selector: 'ngx-echarts-pie-event',
   templateUrl: './echarts-pie-event.component.html',
   styleUrls: ['./echarts-pie-event.component.scss'],
 })
-export class EchartsPieEventComponent implements AfterViewInit, OnDestroy {
+export class EchartsPieEventComponent
+  implements OnInit, AfterViewInit, OnDestroy {
   options: any = {};
   themeSubscription: any;
+  categorias: any = [];
+  data: any = [];
 
-  constructor(private theme: NbThemeService) {}
+  constructor(private theme: NbThemeService, private cat: CategoriaService) {}
 
-  ngAfterViewInit() {
-    this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
-      const colors = config.variables;
-      const echarts: any = config.variables.echarts;
-
-      this.options = {
-        backgroundColor: echarts.bg,
-        color: [
-          colors.warningLight,
-          colors.infoLight,
-          colors.dangerLight,
-          colors.successLight,
-          colors.primaryLight,
-        ],
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)',
-        },
-        legend: {
-          orient: 'vertical',
-          left: 'left',
-          data: ['USA', 'Germany', 'France', 'Canada', 'Russia'],
-          textStyle: {
-            color: echarts.textColor,
+  ngOnInit() {
+    this.cat.getCategoria().subscribe(res => {
+      this.categorias = map(res, 'nome');
+      res.forEach(element => {
+        this.data.push(pick(element, ['nome', 'count']));
+        this.data[this.data.length - 1] = mapKeys(
+          this.data[this.data.length - 1],
+          (value, key) => {
+            return key === 'count' ? 'value' : 'name';
           },
-        },
-        series: [
-          {
-            name: 'Categorias',
-            type: 'pie',
-            radius: '80%',
-            center: ['50%', '50%'],
-            data: [
-              { value: 335, name: 'Germany' },
-              { value: 310, name: 'France' },
-              { value: 234, name: 'Canada' },
-              { value: 135, name: 'Russia' },
-              { value: 1548, name: 'USA' },
-            ],
-            itemStyle: {
-              emphasis: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: echarts.itemHoverShadowColor,
-              },
+        );
+      });
+
+      this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
+        const colors = config.variables;
+        const echarts: any = config.variables.echarts;
+
+        this.options = {
+          backgroundColor: echarts.bg,
+          color: [
+            colors.warningLight,
+            colors.infoLight,
+            colors.dangerLight,
+            colors.successLight,
+            colors.primaryLight,
+          ],
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)',
+          },
+          legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: this.categorias,
+            textStyle: {
+              color: echarts.textColor,
             },
-            label: {
-              normal: {
-                textStyle: {
-                  color: echarts.textColor,
+          },
+          series: [
+            {
+              name: 'Categorias',
+              type: 'pie',
+              radius: '80%',
+              center: ['50%', '50%'],
+              data: this.data,
+              itemStyle: {
+                emphasis: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: echarts.itemHoverShadowColor,
+                },
+              },
+              label: {
+                normal: {
+                  textStyle: {
+                    color: echarts.textColor,
+                  },
+                },
+              },
+              labelLine: {
+                normal: {
+                  lineStyle: {
+                    color: echarts.axisLineColor,
+                  },
                 },
               },
             },
-            labelLine: {
-              normal: {
-                lineStyle: {
-                  color: echarts.axisLineColor,
-                },
-              },
-            },
-          },
-        ],
-      };
+          ],
+        };
+      });
     });
   }
+
+  ngAfterViewInit() {}
 
   ngOnDestroy(): void {
     this.themeSubscription.unsubscribe();
