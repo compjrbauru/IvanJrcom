39a509dcecdc7b35e3d2461e67a0001afc506821
Rennan Eduardo Campoy <rennancampoy@yahoo.com.br>
2018-08-29 20:54:44 -0300

    review na home.module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
-import { UserInfoModule } from './user-info/user-info.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { ThemeModule } from '../@theme-home/theme.module';
 import { MainModule} from './main/main.module';
@@ -8,7 +7,7 @@ import { MainModule} from './main/main.module';
 const HOME_COMPONENTS = [HomeComponent];
 
 @NgModule({
-  imports: [HomeRoutingModule, ThemeModule, MainModule, UserInfoModule],
+  imports: [HomeRoutingModule, ThemeModule, MainModule],
   declarations: [...HOME_COMPONENTS],
 })
 export class HomeModule {}
