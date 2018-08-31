0f811a32153dad45290988a05066b57d1fe68796
vvasc <vinicius.dias@fc.unesp.br>
2018-08-30 23:23:28 -0300

    Fix showCategoria

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,11 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { ThemeModule } from '../@theme-home/theme.module';
 import { MainModule } from './main/main.module';
-import { ShowCategoriaModule } from './show-categoria/show-categoria.module';
 
 const HOME_COMPONENTS = [HomeComponent];
 
 @NgModule({
-  imports: [HomeRoutingModule, ThemeModule, MainModule, ShowCategoriaModule],
+  imports: [HomeRoutingModule, ThemeModule, MainModule],
   declarations: [...HOME_COMPONENTS ],
 })
 export class HomeModule {}
