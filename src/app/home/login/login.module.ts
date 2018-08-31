42bbdbc90d4d73d900f79fd5b649d5642beebb27
vvasc <vinicius.dias@fc.unesp.br>
2018-08-31 09:29:05 -0300

    lint novamente

--- src/app/home/login/login.module.ts
+++ src/app/home/login/login.module.ts
@@ -1,12 +1,11 @@
-import { ResetPassComponent } from './reset-pass.component';
-import { AuthService } from './../../services/auth.service';
 import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material';
 
 import { ThemeModule } from '../../@theme-home/theme.module';
+import { UsuarioService } from '../../services/usuario.service';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-import { UsuarioService } from '../../services/usuario.service';
-import { MatDialogModule } from '@angular/material';
+import { ResetPassComponent } from './reset-pass.component';
 
 const LOGIN_COMPONENTS = [LoginComponent, ResetPassComponent];
 
