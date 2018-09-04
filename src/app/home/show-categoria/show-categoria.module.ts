3c946eb0b5ca939ce7ae6e658fa605ff434f2e24
GustavoTrielli <guga9033@gmail.com>
2018-09-03 23:29:09 -0300

    acertando imports show categoria

--- src/app/home/show-categoria/show-categoria.module.ts
+++ src/app/home/show-categoria/show-categoria.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-
+import { ShowCategoriaRoutingModule } from './show-categoria-routing.module';
 import { ShowCategoriaComponent } from './show-categoria.component';
 import { ThemeModule } from '../../@theme/theme.module';
 
 
 
 @NgModule({
-    imports: [ThemeModule],
+    imports: [ThemeModule, ShowCategoriaRoutingModule],
     declarations: [ShowCategoriaComponent],
   })
   export class ShowCategoriaModule {}
