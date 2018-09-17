40e0739c58fe8dfbc41d3d6331fdffcfadb7fabd
thoffart <thiagohoffart321920@gmail.com>
2018-09-17 11:35:30 -0300

    avancos

--- src/app/@core/components/form-evento/form-evento.component.ts
+++ src/app/@core/components/form-evento/form-evento.component.ts
@@ -1,5 +1,10 @@
 import { Component, DoCheck, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import Geocoder from 'ol-geocoder';
+import TileLayer from 'ol/layer/Tile';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
 
 @Component({
   selector: 'ngx-form-evento',
@@ -16,51 +21,104 @@ export class FormEventoComponent implements OnInit, DoCheck {
   @Output()
   formEmitter = new EventEmitter<any>();
   formEvent: FormGroup = null;
-
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    /*  let geocoder = new Geocoder('nominatim', {
+      provider: 'osm',
+      lang: 'en',
+      placeholder: 'Search for ...',
+      limit: 5,
+      debug: false,
+      autoComplete: true,
+      keepOpen: true,
+    }); */
+    let geocoder = new Geocoder('nominatim', {
+      provider: 'osm',
+      lang: 'en',
+      placeholder: 'Search for ...',
+      limit: 5,
+      debug: false,
+      autoComplete: true,
+      keepOpen: true,
+    });
+    let map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          title: 'Global Imagery',
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+    map.addControl(geocoder);
     const numeroIngressosValidation: ValidatorFn = (form: AbstractControl) => {
-      const error = { numeroIngressos: 'O número de ingressos disponíveis não é a soma de ingressos por gênero' };
+      const error = {
+        numeroIngressos:
+          'O número de ingressos disponíveis não é a soma de ingressos por gênero',
+      };
       // tslint:disable-next-line:max-line-length
-      const soma = +form.get('ingressos').get('feminino').get('disponiveis').value + +form.get('ingressos').get('masculino').get('disponiveis').value + +form.get('ingressos').get('unisex').get('disponiveis').value;
+      const soma =
+        +form
+          .get('ingressos')
+          .get('feminino')
+          .get('disponiveis').value +
+        +form
+          .get('ingressos')
+          .get('masculino')
+          .get('disponiveis').value +
+        +form
+          .get('ingressos')
+          .get('unisex')
+          .get('disponiveis').value;
       // tslint:disable-next-line:max-line-length
-      return +form.get('ingressos').get('lote').get('disponiveis').value === soma ? null : error;
+      return +form
+        .get('ingressos')
+        .get('lote')
+        .get('disponiveis').value === soma
+        ? null
+        : error;
     };
 
-    this.formEvent = this.formBuilder.group({
-      nome: ['', Validators.required],
-      categoria: [null, Validators.required],
-      data: ['', Validators.required],
-      descricao: ['', Validators.required],
-      local: ['', Validators.required],
-      ingressos: this.formBuilder.group({
-        lote: this.formBuilder.group({
-          disponiveis: ['', Validators.required],
-          numero: ['1', Validators.required],
+    this.formEvent = this.formBuilder.group(
+      {
+        nome: ['', Validators.required],
+        categoria: [null, Validators.required],
+        data: ['', Validators.required],
+        descricao: ['', Validators.required],
+        local: ['', Validators.required],
+        ingressos: this.formBuilder.group({
+          lote: this.formBuilder.group({
+            disponiveis: ['', Validators.required],
+            numero: ['1', Validators.required],
+          }),
+          feminino: this.formBuilder.group({
+            disponiveis: [],
+            valor: [],
+          }),
+          masculino: this.formBuilder.group({
+            disponiveis: [],
+            valor: [],
+          }),
+          unisex: this.formBuilder.group({
+            disponiveis: [],
+            valor: [],
+          }),
+          compramax: ['', Validators.required],
         }),
-        feminino: this.formBuilder.group({
-          disponiveis: [],
-          valor: [],
-        }),
-        masculino: this.formBuilder.group({
-          disponiveis: [],
-          valor: [],
-        }),
-        unisex: this.formBuilder.group({
-          disponiveis: [],
-          valor: [],
-        }),
-        compramax: ['', Validators.required],
-      }),
-      mostraHome: null,
-      url: ['', Validators.required],
-      pathurl: ['', Validators.required],
-      id: [''],
-      nomeBusca: null,
-      localBusca: null,
-    },
-    { validator: numeroIngressosValidation });
+        mostraHome: null,
+        url: ['', Validators.required],
+        pathurl: ['', Validators.required],
+        id: [''],
+        nomeBusca: null,
+        localBusca: null,
+      },
+      { validator: numeroIngressosValidation },
+    );
     this.patchValues(this.resolvedEvento);
 
     this.onFormValueChanges();
