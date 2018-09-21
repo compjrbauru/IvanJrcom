d8d41615deb3850a72812be9030f1a56816df206
johndavvid <john.david1321@hotmail.com>
2018-09-20 21:31:25 -0300

    realizada as correções

--- src/app/@core/components/form-evento/form-evento.component.ts
+++ src/app/@core/components/form-evento/form-evento.component.ts
@@ -11,42 +11,53 @@ export class FormEventoComponent implements OnInit, DoCheck {
   @Input()
   categorias: Observable<any>;
   @Input()
-  formReset: boolean;
-  @Input()
   resolvedEvento: any = null;
   @Output()
   formEmitter = new EventEmitter<any>();
   formEvent: FormGroup = null;
-
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     const numeroIngressosValidation: ValidatorFn = (form: AbstractControl) => {
-      const error = { numeroIngressos: 'O número de ingressos disponíveis não é a soma de ingressos por gênero' };
+      const error = {
+        numeroIngressos:
+          'O número de ingressos disponíveis não é a soma de ingressos por gênero',
+      };
       // tslint:disable-next-line:max-line-length
-      const soma = +form.get('ingressos').get('feminino').get('disponiveis').value + +form.get('ingressos').get('masculino').get('disponiveis').value + +form.get('ingressos').get('unisex').get('disponiveis').value;
+      const soma =
+        +form.get('ingressos').get('feminino').get('disponiveis').value +
+        +form.get('ingressos').get('masculino').get('disponiveis').value +
+        +form.get('ingressos').get('unisex').get('disponiveis').value;
       // tslint:disable-next-line:max-line-length
       return +form.get('ingressos').get('lote').get('disponiveis').value === soma ? null : error;
     };
 
-    this.formEvent = this.formBuilder.group({
-      nome: ['', Validators.required],
-      categoria: [null, Validators.required],
-      data: ['', Validators.required],
-      descricao: ['', Validators.required],
-      local: ['', Validators.required],
-      ingressos: this.formBuilder.group({
-        lote: this.formBuilder.group({
-          disponiveis: ['', Validators.required],
-          numero: ['1', Validators.required],
-        }),
-        feminino: this.formBuilder.group({
-          disponiveis: [],
-          valor: [],
-        }),
-        masculino: this.formBuilder.group({
-          disponiveis: [],
-          valor: [],
+    this.formEvent = this.formBuilder.group(
+      {
+        nome: ['', Validators.required],
+        categoria: [null, Validators.required],
+        data: ['', Validators.required],
+        descricao: ['', Validators.required],
+        local: ['', Validators.required],
+        coordenadas: ['', Validators.required],
+        ingressos: this.formBuilder.group({
+          lote: this.formBuilder.group({
+            disponiveis: ['', Validators.required],
+            numero: ['1', Validators.required],
+          }),
+          feminino: this.formBuilder.group({
+            disponiveis: [],
+            valor: [],
+          }),
+          masculino: this.formBuilder.group({
+            disponiveis: [],
+            valor: [],
+          }),
+          unisex: this.formBuilder.group({
+            disponiveis: [],
+            valor: [],
+          }),
+          compramax: ['', Validators.required],
         }),
         unisex: this.formBuilder.group({
           disponiveis: [],
@@ -59,18 +70,18 @@ export class FormEventoComponent implements OnInit, DoCheck {
           vendafisica: [],
         }),
         compramax: ['', Validators.required],
-      }),
-      mostraHome: null,
-      url: ['', Validators.required],
-      pathurl: ['', Validators.required],
-      id: [''],
-      nomeBusca: null,
-      localBusca: null,
-    },
-    { validator: numeroIngressosValidation });
+        mostraHome: null,
+        url: ['', Validators.required],
+        pathurl: ['', Validators.required],
+        id: [''],
+        nomeBusca: null,
+        localBusca: null,
+        }),
+    { validator: numeroIngressosValidation };
     this.patchValues(this.resolvedEvento);
 
     this.onFormValueChanges();
+    this.formEmitter.emit(this.formEvent);
   }
 
   ngDoCheck() {
@@ -100,11 +111,6 @@ export class FormEventoComponent implements OnInit, DoCheck {
         ...resolvedEvento,
       });
     }
-  }
-
-  imagemupdate(event: any) {
-    this.formEvent.controls['url'].setValue(event.url);
-    this.formEvent.controls['pathurl'].setValue(event.pathurl);
   }
 
   onFormValueChanges() {
--- src/app/@core/components/form-evento/form-evento.module.ts
+++ src/app/@core/components/form-evento/form-evento.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { UploadFileModule } from '../upload-file/upload-file.module';
 import { ThemeModule } from './../../../@theme-home/theme.module';
 import { FormEventoComponent } from './form-evento.component';
 
 @NgModule({
-  imports: [ThemeModule, CommonModule, UploadFileModule],
+  imports: [ThemeModule, CommonModule],
   declarations: [FormEventoComponent],
   exports: [FormEventoComponent],
 })
