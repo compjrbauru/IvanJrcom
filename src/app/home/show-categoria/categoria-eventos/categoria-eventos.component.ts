426aa654a8cca057b3a234a82eae74f1541ab7ff
thoffart <thiagohoffart321920@gmail.com>
2018-09-27 20:49:14 -0300

    #160217555: feature finalizada

---
+++ src/app/home/show-categoria/categoria-eventos/categoria-eventos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { EventoService } from './../../../services/evento.service';
+
+@Component({
+  selector: 'ngx-categoria-eventos',
+  templateUrl: './categoria-eventos.component.html',
+  styleUrls: ['./categoria-eventos.component.scss']
+})
+export class CategoriaEventosComponent implements OnInit {
+  eventosAsync: Observable<any>;
+
+  constructor(private route: ActivatedRoute, private eventoservice: EventoService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(res => {
+      this.eventosAsync = this.eventoservice.getCategoria(res.categoria);
+    });
+  }
+
+
+
+}
