66b6f458bc404e68f88dcb7b3f07149625e8caab
thoffart <thiagohoffart321920@gmail.com>
2018-10-04 01:03:37 -0300

    rota para a conta quando login completado com sucesso adicionada

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnChanges } from '@angular/core';
-import { Router } from '@angular/router';
-import { NbMenuItem, NbMenuService } from '@nebular/theme';
+import { Component } from '@angular/core';
 
-import { AuthService } from './../services/auth.service';
 import { MENU_ITEMS } from './home-menu';
 
 @Component({
@@ -10,27 +7,9 @@ import { MENU_ITEMS } from './home-menu';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnChanges {
+export class HomeComponent {
   menu = MENU_ITEMS;
-  conta: NbMenuItem[] = [{
-    title: 'Conta',
-    icon: 'nb-email',
-    link: '/home/conta',
-    home: true,
-  }];
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private nbMenuService: NbMenuService,
-  ) {}
 
-  ngOnChanges() {
-    this.authService.token ?
-      this.nbMenuService.addItems(this.conta) :
-      null;
-   }
+  constructor() { }
 
-  changeAdmin() {
-    this.router.navigate(['/admin']);
-  }
 }
