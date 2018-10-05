9691c2ff87f06ca32b0f4373202562b4341660e8
thoffart <thiagohoffart321920@gmail.com>
2018-10-05 16:01:59 -0300

    #160973071: recapatcha funcionando

--- src/app/home/page-evento/pagamento/deposito-confirmation/deposito-confirmation.component.ts
+++ src/app/home/page-evento/pagamento/deposito-confirmation/deposito-confirmation.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ngx-deposito-confirmation',
-  templateUrl: './deposito-confirmation.component.html',
-  styleUrls: ['./deposito-confirmation.component.scss']
+  template:
+    `
+  <div>
+  <button [disabled]="!valid" class="btn btn-hero-success btn-demo">Comprar</button>
+  </div>
+  `,
 })
 export class DepositoConfirmationComponent implements OnInit {
-
+  @Input() valid: any;
   constructor() { }
 
   ngOnInit() {
