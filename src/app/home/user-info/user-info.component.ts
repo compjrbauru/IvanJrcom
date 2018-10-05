13bdb18f89781bf43477d24ae5e3bab81d41fc7b
thoffart <thiagohoffart321920@gmail.com>
2018-10-05 16:35:38 -0300

    #161028418: avanços

--- src/app/home/user-info/user-info.component.ts
+++ src/app/home/user-info/user-info.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './../../services/auth.service';
+import { UsuarioService } from './../../services/usuario.service';
+
 @Component({
   selector: 'ngx-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss'],
 })
 export class UserInfoComponent implements OnInit {
+  userInfo: any;
 
-  constructor() { }
+  constructor(private usuarioservice: UsuarioService, private authservice: AuthService) { }
 
   ngOnInit() {
+
+    this.usuarioservice.getUsuarioEmail(this.authservice.ReturnEmail()).subscribe(res => {
+      this.userInfo = res[0];
+    });
   }
 
 }
