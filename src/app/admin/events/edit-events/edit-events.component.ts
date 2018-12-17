eedf5fcb03e7b059ac058b1af3bca81f3677226b
vvasc <vinedias@gmail.com>
2018-12-17 20:02:15 -0200

    162532108: Apagando list events e usando edit-events

--- src/app/admin/events/edit-events/edit-events.component.ts
+++ src/app/admin/events/edit-events/edit-events.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
+import { tap, switchMap, filter } from 'rxjs/operators';
+import { UploadFileComponent } from '../../../@core/components/upload-file/upload-file.component';
+import { MapComponent } from '../../../@core/components/map/map.component';
+import { EventoService } from '../../../services/evento.service';
+import { QueryService } from '../../../services/query.service';
+import { CategoriaService } from '../../../services/categoria.service';
+import { DepositoService } from '../../../services/deposito.service';
+import { NotificacaoService } from '../../../services/notificacao.service';
+import { ConfirmationModalComponent } from '../../../@core/components/confirmation-modal/confirmation-modal.component';
+
 
 @Component({
   selector: 'ngx-edit-events',
@@ -6,10 +18,123 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-events.component.scss'],
 })
 export class EditEventsComponent implements OnInit {
+  form: any = {};
+  formDeleteValue: any = [];
+  categorias: any;
+  categoria: any;
+  eventoAsync: Observable<any>;
+  eventoResolver: any = [];
+  dependencies: any;
+  update: any;
+  @ViewChild(UploadFileComponent) private upload: UploadFileComponent;
+  @ViewChild(MapComponent) private map: MapComponent;
+  categoriaSelected: any = {};
 
-  constructor() { }
+  constructor(
+    private eventoService: EventoService,
+    private queryService: QueryService,
+    private categoriaService: CategoriaService,
+    public dialog: MatDialog,
+    private depositoservice: DepositoService,
+    private notificacaoService: NotificacaoService,
+  ) { }
 
   ngOnInit() {
+    this.eventoAsync = this.eventoService.getAll();
+    const categoriaAsync = this.categoriaService.getCategoria();
+    const depositoAsync = this.depositoservice.getContaDeposito();
+    this.dependencies = { categoria: categoriaAsync, deposito: depositoAsync };
   }
 
+  resolver(event) {
+    this.eventoResolver = event;
+  }
+
+  submit(form: any) {
+    form.data = new Date(form.data);
+    form.nomeBusca = form.nome.toLowerCase();
+    form.localBusca = form.local.toLowerCase();
+    if (form.pathurl !== this.eventoResolver.pathurl) {
+      this.queryService.deleteImage(this.eventoResolver.pathurl).subscribe();
+    }
+    this.update = this.eventoService.patchData(form, this.eventoResolver.id);
+    this.categoriaService.patchEditCategoria(form, this.eventoResolver);
+
+    this.notificacaoService.ngxtoaster('Aviso', 'Evento editado com sucesso!', true);
+
+    this.eventoResolver = [];
+    this.form['formEvent'].reset();
+    this.upload.resetUpload();
+    this.map.resetMap();
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (
+      this.form['formEvent'].value.pathurl !== '' &&
+      this.form['formEvent'].value.pathurl !== this.eventoResolver.pathurl
+    ) {
+      const dialogRef = this.dialog.open(ConfirmationModalComponent, {
+        width: '40%',
+        data: {
+          header: 'Aviso!',
+          text: 'Você enviou uma imagem, tem certeza que deseja sair?',
+        },
+        disableClose: true,
+      });
+      return dialogRef.afterClosed().pipe(
+        tap(res => {
+          if (res === true) {
+            this.queryService.deleteImage(this.form['formEvent'].value.pathurl).subscribe();
+          }
+        }),
+      );
+    } else {
+      return true;
+    }
+  }
+
+  deleteEvento(formValue: any) {
+    this.formDeleteValue = formValue;
+    const dialogRef = this.dialog.open(ConfirmationModalComponent, {
+      width: '40%',
+      data: {
+        header: 'Aviso!',
+        text: 'Se você excluir o evento excluirá todos os registros ligados à este evento como: ingressos, imagens e registros de compra, tem certeza que deseja excluir?',
+      },
+      disableClose: true,
+    });
+    dialogRef.afterClosed().pipe(
+      filter(response => response === true),
+      switchMap(() => this.categoriaService.getById(this.formDeleteValue.categoria)),
+      tap(this.patchDeleteCategoria),
+      tap(this.deleteImage),
+      switchMap(() => this.eventoService.removeDataCascade(this.formDeleteValue.id)),
+    ).subscribe(() => {
+      this.eventoService.removeData(this.formDeleteValue.id);
+    });
+  }
+
+  private patchDeleteCategoria = (categoria: any): void => {
+    this.categoriaService.patchDeleteEventCategoria(categoria, this.formDeleteValue);
+  }
+
+  private deleteImage = (): void => {
+    if (this.form['formEvent'].value.pathurl !== '' && this.form['formEvent'].value.pathurl !== this.eventoResolver.pathurl) {
+      this.queryService.deleteImage(this.formDeleteValue.pathurl).pipe(
+        switchMap(() => this.queryService.deleteImage(this.eventoResolver.pathurl)),
+      ).subscribe();
+    } else {
+      this.queryService.deleteImage(this.eventoResolver.pathurl).subscribe();
+    }
+  }
+
+  mapUpdate(event: any) {
+    this.form['formEvent'].controls['local'].setValue(event.local);
+    this.form['formEvent'].controls['coordenadas'].setValue(event.coordenadas);
+  }
+
+  imagemupdate(event: any) {
+    this.form['formEvent'].controls['url'].setValue(event.url);
+    this.form['formEvent'].controls['pathurl'].setValue(event.pathurl);
+  }
 }
