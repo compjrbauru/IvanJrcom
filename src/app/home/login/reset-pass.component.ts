a97104e756cfae0e57f25b90b218d6a6b3cc50c6
GustavoTrielli <guga9033@gmail.com>
2018-12-20 18:35:08 -0200

    alterações da review

--- src/app/home/login/reset-pass.component.ts
+++ src/app/home/login/reset-pass.component.ts
@@ -15,32 +15,30 @@ import { MatDialogRef } from '@angular/material';
     </div>
     <div class="modal-body">
         <label for="email">E-mail</label>
-        <input [formControl]= "email" id="email" type="email" required
+        <input [formControl]= "email" id="email" type="email"
          placeholder="E-mail" class="form-control"/>
     </div>
     <div class="modal-footer">
-        <button class="btn btn-md btn-primary" [disabled]="!email.valid" (click)="resetPass(email)">Resetar Senha</button>
+        <button class="btn btn-md btn-primary" [disabled]="!email.valid" (click)="resetPass(email.value)">Resetar Senha</button>
     </div>
   `,
   providers: [NotificacaoService],
 })
 export class ResetPassComponent {
-  email: FormControl;
+  email = new FormControl('', Validators.compose([Validators.required, Validators.email]));
 
   constructor(
     public dialogRef: MatDialogRef<any>,
     private authService: AuthService,
     private notificacao: NotificacaoService,
-  ) {
-    this.email = new FormControl('', Validators.email);
-  }
+  ) { }
 
   closeDialog() {
     this.dialogRef.close();
   }
 
-  resetPass() {
-    this.authService.resetPassword(this.email.value).then(res => {
+  resetPass(email: string) {
+    this.authService.resetPassword(email).then(res => {
       if (res === 'success') {
         this.notificacao.ngxtoaster('Resetar Senha', 'Instrucoes enviadas para o Email!', true);
       } else if (res === 'auth/user-not-found') {
